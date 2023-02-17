1addd4b890ca592502608d2d7d904b9c41b6036c
Amytechie <amyukoha36@gmail.com>
2023-02-16 17:30:12 -0800

    omo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /* this is the input command */
 int main(void)
+{
 /* this is the input function */
 	puts("Programming is like building a multilingual puzzle\n");
 /* prints the function followed by a new line */
 	return (0);
-/* end of C program */
+}
