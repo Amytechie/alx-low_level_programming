1db2a7fd0e54d8b97afe691ab7f363e40bf408f5
Amytechie <amyukoha36@gmail.com>
2023-02-16 18:44:40 -0800

    chains

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - this is the function command
+ * main -  this is the function command
  *
- * Return: value is constant 0
+ *Return: value is constant 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
