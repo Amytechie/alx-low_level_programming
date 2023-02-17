b0f54f18b0fbd8c637c0bd9a736cbc6b099681e4
Amytechie <amyukoha36@gmail.com>
2023-02-17 02:40:31 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - this is the main function
  *
  * Return: value is constant 0
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
