57679407b8a8cb770e4f69b038f63ac8bfcfe576
Amytechie <amyukoha36@gmail.com>
2023-02-17 03:37:36 -0800

    last size change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	printf("size of a char: %lu byte(s)", sizeof(char));
-	printf("size of an int: %lu byte(s)", sizeof(int));
-	printf("size of a short: %lu byte(s)", sizeof(short));
-	printf("size of a long: %lu byte(s)", sizeof(long));
-	printf("size of a float: %lu byte(s)", sizeof(float));
-	printf("size of a double: %lu byte(s)", sizeof(double));
-	printf("size of a long double: %lu byte(s)", sizeof(long double));
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a short: %lu byte(s)\n", sizeof(short));
+	printf("size of a long: %lu byte(s)\n", sizeof(long));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	printf("size of a double: %lu byte(s)\n", sizeof(double));
+	printf("size of a long double: %lu byte(s)\n", sizeof(long double));
 	return (0);
 }
