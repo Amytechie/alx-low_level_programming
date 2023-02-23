d892b647965dce1b8bd93ed2aaf8f91ddc4e6852
Amytechie <amyukoha36@gmail.com>
2023-02-23 08:48:57 -0800

    collaboration

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+extern int _putchar(char c);
+
+/**
+ * print_numbers - prints rumbers
+ * @void: null
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+	
+}
