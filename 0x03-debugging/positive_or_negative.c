d4dbddc93b26ab8a879d7389067410d9079f4ece
Amytechie <amyukoha36@gmail.com>
2023-02-23 01:14:35 -0800

    pos.or_neg.

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * positive_or_negative - tests for positive or negative
+ *
+ * @i: this is the variable
+ */
+
+void positive_or_negative(int i)
+{
+	if (i + 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is zero", i);
+	printf("\n");
+}
