5a0a15eca97d5a7ab5712df169cd488a4a208508
amytechie <amyukoha36@gmail.com>
2023-02-24 14:27:41 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - this is the function
+ * Return: value is 0
+ */
+
+int main(void)
+{
+	long int num = 612852475143;
+	int largest_f = 1;
+	int i;
+
+	while (num % 2 == 0)
+	{
+		largest_f = 2;
+		num /= 2;
+	}
+	for (i = 3; i <= sqrt(num); i += 2)
+	{
+		while (num % i == 0)
+		{
+			largest_f = i;
+			num /= i;
+		}
+	}
+	if (num > 2)
+	{
+		largest_f = num;
+	}
+
+	printf("%d\n", largest_f);
+
+	return (0);
+}
