34bd6741a534455b02442fa59b48269fc1d15b3e
amytechie <amyukoha36@gmail.com>
2023-03-06 15:43:20 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strstr - the main function
+ * @haystack: first pointer
+ * @needle: second pointer
+ * Return: return value
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
